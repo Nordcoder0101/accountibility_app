b86c44a8b4d0f933a0a5e58c9c6ff0dcd1a6a796
Greg <greg@Gregs-MacBook-Air.local>
2018-11-19 11:52:41 -0800

    most jquery functionality built for adding agreements

--- apps/to_dos/static/to_dos/to_dos_jquery.js
+++ apps/to_dos/static/to_dos/to_dos_jquery.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-  $(".add_daily").before('<input type="text" name="daily">')
+  
+  $(".add_agreement").on('click', function(){
+    $(".add_agreement").before(`<input class ="full_line" placeholder = "What you will do..." size="50" type="text" name="daily"> <br /> <select class="length_of_agreement"> <option value = "daily">Daily</option> <option value = "weekly">Weekly</option> <option value = "monthly">Monthly</option> <option class="long_term" value ="long_term">Long Term</option></select> <br />`)
+    
+  })
+  $(document).on('change', ".length_of_agreement", function(){
+      if ($(this).val() == "long_term"){
+        $(this).before('<input class="full_line due_date" type="date" name="due_date"')
+    } 
+      if ($(this).val() != "long_term" && ($(this).prev().attr("class", "due_date"))){
+        $(this).prev().remove()
+      }
+  })
+  
 })

eced100d263e22ac66a13d72ed08b71e427c2105
Alexandru Popa <alexandru.popa0392@gmail.com>
2018-11-19 13:24:10 -0800

    log reg updated...main page updated

--- apps/to_dos/views.py
+++ apps/to_dos/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    # return render(request, )
-    pass
+    return render(request, 'to_dos/landing.html')
 
 def view_profile(request):
     return render(request, 'to_dos/profile.html')

1d65aed218d2c9901fbaf227576dc2cb6e250c3c
Greg <greg@Gregs-MacBook-Air.local>
2018-11-20 10:59:09 -0800

    committing without DB and ajax call for adding agreement is working

--- apps/to_dos/urls.py
+++ apps/to_dos/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^$', views.index),\
+  url(r'^$', views.index),
   url(r'^/profile$', views.view_profile),
   url(r'^/render_agreement$', views.render_agreement),
-  url(r'^/add_agreement$', views.add_profile)
+  url(r'^/add_agreement$', views.add_agreement),
+  url(r'^/render_due_date$', views.render_due_date),
+  url(r'^/delete/(?P<id>[0-9]+)', views.delete_agreement),
+  
 
 ]

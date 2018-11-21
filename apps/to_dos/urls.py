d39f19e3057f4d86d8756a67a6c36c6c65eb0887
Greg <greg@Gregs-MacBook-Air.local>
2018-11-20 19:41:00 -0800

    checkbox updating works

--- apps/to_dos/urls.py
+++ apps/to_dos/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
   url(r'^/render_agreement$', views.render_agreement),
   url(r'^/add_agreement$', views.add_agreement),
   url(r'^/render_due_date$', views.render_due_date),
-  url(r'^/delete/(?P<id>[0-9]+)', views.delete_agreement),
+  url(r'^/delete/(?P<id>[0-9]+)$', views.delete_agreement),
+  url(r'^/is_complete/(?P<id>[0-9]+)$', views.toggle_complete),
+  url(r'^/is_not_complete/(?P<id>[0-9]+)$', views.toggle_not_complete),
   
 
 ]
